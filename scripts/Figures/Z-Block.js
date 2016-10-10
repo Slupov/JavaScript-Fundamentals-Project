16e89de70617e5060ec55d686c25b6af6d87e08e
Maria Velikova <mimzi333@gmail.com>
2016-10-10 18:35:18 +0300

    Z-block rotate

--- scripts/Figures/Z-Block.js
+++ scripts/Figures/Z-Block.js
@@ -19,3 +19,85 @@ ZBlock.prototype = Object.create(Figure.prototype);
 
 ZBlock.prototype.constructor = ZBlock;
 
+ZBlock.prototype.rotate = function(board) {
+    board.matrix[this.blocks[0].yCoordinate][this.blocks[0].xCoordinate] = 0;
+    board.matrix[this.blocks[1].yCoordinate][this.blocks[1].xCoordinate] = 0;
+    board.matrix[this.blocks[2].yCoordinate][this.blocks[2].xCoordinate] = 0;
+    board.matrix[this.blocks[3].yCoordinate][this.blocks[3].xCoordinate] = 0;
+    //parent.position == 0 checks if the line element is in lying position
+    if(parent.position == 0 && this.canRotate(parent.position, board)) {
+        this.blocks[0].xCoordinate = this.blocks[0].xCoordinate + 1;
+        this.blocks[0].yCoordinate = this.blocks[0].yCoordinate + 1;
+        this.blocks[3].xCoordinate = this.blocks[3].xCoordinate + 0;
+        this.blocks[3].yCoordinate = this.blocks[3].yCoordinate - 2;
+        this.blocks[2].xCoordinate = this.blocks[2].xCoordinate + 1;
+        this.blocks[2].yCoordinate = this.blocks[2].yCoordinate - 1;
+        let difference = this.blocks[3].yCoordinate - 3;
+        if(difference < 0) {
+            this.blocks[0].yCoordinate = this.blocks[0].yCoordinate - difference;
+            this.blocks[1].yCoordinate = this.blocks[1].yCoordinate - difference;
+            this.blocks[2].yCoordinate = this.blocks[2].yCoordinate - difference;
+            this.blocks[3].yCoordinate = this.blocks[3].yCoordinate - difference;
+        }
+        parent.position = 1;
+    }
+    //parent.position == 1 checks if the line element is in standing position
+    else if(parent.position == 1 && this.canRotate(parent.position, board)){
+        this.blocks[0].xCoordinate = this.blocks[0].xCoordinate - 1;
+        this.blocks[0].yCoordinate = this.blocks[0].yCoordinate - 1;
+        this.blocks[3].xCoordinate = this.blocks[3].xCoordinate - 0;
+        this.blocks[3].yCoordinate = this.blocks[3].yCoordinate + 2;
+        this.blocks[2].xCoordinate = this.blocks[2].xCoordinate - 1;
+        this.blocks[2].yCoordinate = this.blocks[2].yCoordinate + 1;
+        let difference = this.blocks[3].xCoordinate - 3;
+        if(difference < 0) {
+            this.blocks[0].xCoordinate = this.blocks[0].xCoordinate - difference;
+            this.blocks[1].xCoordinate = this.blocks[1].xCoordinate - difference;
+            this.blocks[2].xCoordinate = this.blocks[2].xCoordinate - difference;
+            this.blocks[3].xCoordinate = this.blocks[3].xCoordinate - difference;
+        }
+        parent.position = 0;
+    }
+
+    board.matrix[this.blocks[0].yCoordinate][this.blocks[0].xCoordinate] = this.blocks[0];
+    board.matrix[this.blocks[1].yCoordinate][this.blocks[1].xCoordinate] = this.blocks[1];
+    board.matrix[this.blocks[2].yCoordinate][this.blocks[2].xCoordinate] = this.blocks[2];
+    board.matrix[this.blocks[3].yCoordinate][this.blocks[3].xCoordinate] = this.blocks[3];
+};
+
+ZBlock.prototype.canRotate = function (position, board) {
+    //parent.position == 0 checks if the line element is in lying position
+    let canRotateInThisPosition = true;
+    let pivotBlockNumber = 1;
+    let pivotBlockY = this.blocks[pivotBlockNumber].yCoordinate;
+    let pivotBlockX = this.blocks[pivotBlockNumber].xCoordinate;
+    if(position == 0) {
+        for(let i = 0; i < this.blocks.length; i++) {
+            if(i == pivotBlockNumber) {
+                continue;
+            }
+
+            if(pivotBlockY - pivotBlockNumber + i >= 0 &&
+                board.matrix[pivotBlockY - pivotBlockNumber + i][pivotBlockX] != EMPTY_CELL){
+                canRotateInThisPosition = false;
+                break;
+            }
+        }
+    }
+    //parent.position == 1 checks if the line element is in standing position
+    else if(position == 1) {
+        for(let i = 0; i < this.blocks.length; i++) {
+            if(i == pivotBlockNumber) {
+                continue;
+            }
+
+            if(pivotBlockX - pivotBlockNumber + i >= 0 &&
+                board.matrix[pivotBlockY][pivotBlockX  - pivotBlockNumber + i] != EMPTY_CELL){
+                canRotateInThisPosition = false;
+                break;
+            }
+        }
+    }
+
+    return canRotateInThisPosition;
+};
